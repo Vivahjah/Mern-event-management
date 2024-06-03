e8f13851e943bdfa5ed9dedc6cfc5ad90cc79db6
Mengwa Ugo <mengwa44@yahoo.com>
2024-06-03 01:13:46 +0100

    setting up the header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href={"/"} className="w-36">
+          <Image
+            src={"/assets/images/logo.svg"}
+            alt="Evently-logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
